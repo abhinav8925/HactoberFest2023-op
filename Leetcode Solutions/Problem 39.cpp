1f1a9f3058d27b320a42782b3f33ef28254917ce
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-10-13 00:45:22 +0530

    Create Problem 39.cpp

---
+++ Leetcode Solutions/Problem 39.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    void comb(int ind, int n, int target, vector<int> &ds, vector<int> &a, vector<vector<int>> &ans) {
+    if (ind == n) {
+        if (target == 0) {
+            ans.push_back(ds);
+        }
+        return;
+    }
+
+    if (a[ind] <= target) {
+        ds.push_back(a[ind]);
+        comb(ind, n, target - a[ind], ds, a, ans);
+        ds.pop_back();
+    }
+    comb(ind + 1, n, target, ds, a, ans); 
+}
+
+vector<vector<int>> combinationSum(vector<int>& a, int target) {
+    int n = a.size();
+    vector<vector<int>> ans;
+    vector<int> ds;
+    
+    comb(0, n, target, ds, a, ans);
+    return ans;
+}
+
+};
